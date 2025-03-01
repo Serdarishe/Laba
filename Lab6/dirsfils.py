400cd63eee67091ac9e9e36a5250b0a93df5b20d
Serdar <serdardundar007@gmail.com>
2025-03-01 14:31:50 +0500

    posledniy shtrih

--- Lab6/dirsfils.py
+++ Lab6/dirsfils.py
@@ -33,3 +33,57 @@ if(os.access(path, os.F_OK)):
 else:
     print('Exist:', os.access(path, os.F_OK))
 
+#4
+papapa = 'lab6/papapa.txt'
+
+file = open(papapa)
+cnt = 0
+for i in file:
+    cnt +=1
+
+print(cnt)
+
+#5
+lst = [0,1,2,3,4,5,6,7,8,9]
+
+file = 'lab6/task5.txt'
+with open(file,'w') as f:
+    for i in lst:
+        f.write(str(i)  + "\n" +((i+1) *" "))
+
+#6
+for i in range(65,91):
+    f = chr(i)
+    file = f"lab6/{f}.txt"
+    try:
+        with open(file, 'x') as f:
+            print(f"file {file} created!")
+    except FileExistsError:
+        print(f"file {file} already exists!")
+
+#7
+file1 = 'lab6/papapa.txt'
+file2 = 'lab6/A.txt'
+
+with open(file1, 'r') as f1:
+    data = f1.read()
+
+with open(file2, 'a') as f2:
+    f2.write(data)
+
+#8
+def deldel(file):
+    if not os.path.exists(file):
+        print (f"{file} doesn't exist")
+        return
+    if not os.access(file , os.W_OK):
+        print(f"can't access {file}")
+        return
+    try:
+        os.remove(file)
+        print(f"{file} deleted")
+    except Exception:
+        print(f"can't delete {file}")
+
+file = 'lab6/file.txt'
+deldel(file)
